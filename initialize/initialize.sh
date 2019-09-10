891ff9299fadec8536e3bd6b614066cd94974b96
Rob deCarvalho <robdmc@gmail.com>
2019-09-09 21:39:14 -0400

    added update_environment command

--- initialize/initialize.sh
+++ initialize/initialize.sh
@@ -1,17 +1,15 @@
 #! /usr/bin/env bash
 
+compose_file="../docker-compose.yml"
+
 # Nuke and recreate the docker volume
-docker-compose down || true
-
-docker volume rm easyenv_opt  || true
-docker volume rm easyenv_ssh  || true
-docker volume create easyenv_opt
-docker volume create easyenv_ssh
-
+docker-compose -f $compose_file down || true
 
 # Install conda into the container
-docker-compose run --rm shell bash /docker/install_conda.sh
+bash update_environment.sh
 
-# Set up a unique ssh key
-docker-compose run --rm shell ssh-keygen -q -t rsa -N '' -f /root/.ssh/id_rsa
+# Initialize ssh
+docker volume rm easyenv_ssh  || true
+docker volume create easyenv_ssh
+docker-compose -f $compose_file run --rm shell ssh-keygen -q -t rsa -N '' -f /root/.ssh/id_rsa
 
