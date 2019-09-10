e42861d326c75645510aef10e71f21bba41ea37f
Rob deCarvalho <robdmc@gmail.com>
2019-09-10 10:20:35 -0400

    more tweaking of install files

--- initialize/container_scripts/build_environment.sh
+++ initialize/container_scripts/build_environment.sh
@@ -1,3 +1,3 @@
 # Install conda
-conda env create --force -f /root/environment.yml
-
+# conda env create --force -f /docker/initialize/environment.yml
+conda env update  --file /docker/initialize/environment.yml
--- initialize/initialize.sh
+++ initialize/initialize.sh
@@ -2,16 +2,20 @@
 
 compose_file="../docker-compose.yml"
 
-# Install conda into the container
-docker-compose -f $compose_file down || true
+# Prepare volumes
 docker volume rm easyenv_opt  || true
 docker volume create easyenv_opt
+
+docker volume rm easyenv_ssh  || true
+docker volume create easyenv_ssh
+
+
+# Install conda into the container
+docker-compose -f $compose_file down || true
 docker-compose -f $compose_file run --rm shell bash /docker/initialize/container_scripts/install_conda.sh
 docker-compose -f $compose_file run --rm shell bash /docker/initialize/container_scripts/build_environment.sh
 
 # Initialize ssh
 docker-compose -f $compose_file down || true
-docker volume rm easyenv_ssh  || true
-docker volume create easyenv_ssh
 docker-compose -f $compose_file run --rm shell ssh-keygen -q -t rsa -N '' -f /root/.ssh/id_rsa
 
