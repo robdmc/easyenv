c04d5a961a285e1c87e098a931e9a6d405abf4f5
Rob deCarvalho <robdmc@gmail.com>
2019-09-11 21:51:48 -0400

    updated initialize.bat

--- initialize/initialize.bat
+++ initialize/initialize.bat
@@ -1,5 +1,20 @@
-rem Reinitialize a clean docker volume and install conda
-cmd.exe /c docker volume erase easyenv_opt
-docker volume create easyenv_opt
-docker pull robdmc/easyenv:latest
-docker-compose run --rm shell bash /build/install_conda.sh
+set compose_file="..\docker-compose.yml"
+
+docker-compose -f %compose_file% down  -v
+docker-compose -f %compose_file% down
+
+
+rem Install conda into the container
+docker-compose -f %compose_file% run --rm shell bash /docker/initialize/container_scripts/install_conda.sh
+docker-compose -f %compose_file% run --rm shell bash /docker/initialize/container_scripts/build_environment.sh
+
+rem Initialize ssh
+docker-compose -f %compose_file% run --rm shell ssh-keygen -q -t rsa -N '' -f /root/.ssh/id_rsa
+
+rem Make start scripts
+echo "docker-compose run --rm shell" > ..\start_shell.bat
+echo "docker-compose run --service-ports --rm notebook" > ..\start_notebook.bat
+
+pause
+
+
--- initialize/initialize.sh
+++ initialize/initialize.sh
@@ -13,11 +13,12 @@ docker-compose -f $compose_file run --rm shell bash /docker/initialize/container
 # Initialize ssh
 docker-compose -f $compose_file run --rm shell ssh-keygen -q -t rsa -N '' -f /root/.ssh/id_rsa
 
+# Make start scripts
 echo "#! /usr/bin/env bash" > ../start_shell.sh
 echo "docker-compose run --rm shell" >> ../start_shell.sh
-
 echo "#! /usr/bin/env bash" > ../start_notebook.sh
 echo "docker-compose run --service-ports --rm notebook" >> ../start_notebook.sh
 
+# Ensure start scripts are executable
 chmod +x ../start_shell.sh
 chmod +x ../start_notebook.sh
